58f90c7d34ca74858041ba1deb96df0375c0ae1a
lbkai <41504146+lbkai@users.noreply.github.com>
2021-11-16 11:13:37 +0800

    Update Test.java

--- com.demo/src/com/demo/Test.java
+++ com.demo/src/com/demo/Test.java
@@ -3,9 +3,7 @@ package com.demo;
 import java.util.ArrayList;
 
 public class Test {
-      String str = "00";
       Object ob = new Object();
       
       ArrayList al = new ArrayList();
-      
 }
